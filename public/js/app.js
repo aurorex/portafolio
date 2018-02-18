0e05475928cb1695c9beaf3840b418c069085c19
aurorex <selva_beat@hotmail.com>
2018-02-18 21:28:42 +0100

    modificando css,agrgando imagenes

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 (function($) {
   let listContact = $('div.contacts-list>div>a');
-
+  // let title = $('h1.text-center');
   // evento para el elemento listContact
   listContact.on('mouseover', function() {
     $(this).css({'font-size': '20px', '-webkit-transition': '0.2s', 'color':'rgb(55,8,42)', 'cursor':'pointer'});
@@ -12,4 +12,22 @@
     $('div.contacts-list>div>a>i').css({'color':'rgb(45, 107, 73)'});
   });
 
+  // $(window).scroll(function() {
+  //   if ($(window).scrollTop() > 100) {
+  //     title.fadeOut();
+  //   } else {
+  //     title.fadeIn();
+  //   }
+  // });
+  // $('#inicio').on('click', function() {
+  //   var posicion = $('#final').offset().top;
+  //   $('html, body').animate({
+  //     scrollTop: posicion
+  //   }, 2000); 
+  // });
+
+  // listAnchor.on('click', function() {
+    
+  // })
+  
 })(jQuery);
