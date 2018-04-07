6f5814c3a126e366df8d39017a4da7565194e715
aurorex <selva_beat@hotmail.com>
2018-04-07 07:33:38 -0500

    agregando efectos scroll portafolio

--- public/js/app.js
+++ public/js/app.js
@@ -1,40 +1,71 @@
-(function($) { 
-  
-  // let listContact = $('div.contact-list>div>a>i');
+(function($) {
+ 
+ // let listContact = $('div.contact-list>div>a>i');
 
-  // listContact.on('mouseover', function() {
-  //   $(this).css({'font-size': '78px', '-webkit-transition': '0.2s', 'color':'rgb(38, 30, 65)', 'cursor':'pointer'});
-  //   // $('div.contacts-list>div>a>i').css({'color':'rgb(38, 30, 65)'});
-  // });
+ // listContact.on('mouseover', function() {
+ //   $(this).css({'font-size': '78px', '-webkit-transition': '0.2s', 'color':'rgb(38, 30, 65)', 'cursor':'pointer'});
+ //   // $('div.contacts-list>div>a>i').css({'color':'rgb(38, 30, 65)'});
+ // });
 
-  // listContact.on('mouseout', function() {
-  //   $(this).css({'font-size': '70px',  '-webkit-transition': '0.2s', 'color':'rgb(209, 208, 208); '});
-  //   // $('div.contacts-list>div>a>i').css({'color':'rgb(38, 30, 65)'});
-  // });
+ // listContact.on('mouseout', function() {
+ //   $(this).css({'font-size': '70px',  '-webkit-transition': '0.2s', 'color':'rgb(209, 208, 208); '});
+ //   // $('div.contacts-list>div>a>i').css({'color':'rgb(38, 30, 65)'});
+ // });
 
-  // $(window).scroll(function() {
-  //   if ($(window).scrollTop() > 100) {
-  //     title.fadeOut();
-  //   } else {
-  //     title.fadeIn();
-  //   }
-  // });
+ // $(window).scroll(function() {
+ //   if ($(window).scrollTop() > 100) {
+ //     title.fadeOut();
+ //   } else {
+ //     title.fadeIn();
+ //   }
+ // });
 
-  let functionScroll = (start, end) => {
-    $(start).on('click', function() {
-      var posicion = $(end).offset().top;
-      $('html, body').animate({
-        scrollTop: posicion
-      }, 1000); 
-    });
-  };
-  functionScroll('#homepage-start','#homepage');
-  functionScroll('#aboutme-start','#aboutme');
-  functionScroll('#skills-start','#skills');
-  functionScroll('#portfolio-start','#portfolio');
-  functionScroll('#contact-start','#contact');
-  functionScroll('#coverpage-start', '#coverpage');
-  functionScroll('#icon1-contacts','#contacts');
-  functionScroll('#icon2-contacts','#contacts');
+ let functionScroll = (start, end) => {
+   $(start).on('click', function() {
+     var posicion = $(end).offset().top;
+     $('html, body').animate({
+       scrollTop: posicion
+     }, 1000);
+   });
+ };
+
+ var controller = new ScrollMagic.Controller();
+
+ new ScrollMagic.Scene({
+   triggerElement:'#aboutme'
+ })
+ .setClassToggle('#aboutme','show')
+ .addTo(controller);
+
+ new ScrollMagic.Scene({
+   triggerElement:'#skills'
+ })
+ .setClassToggle('#skills','show')
+ .addTo(controller);
+
+
+ new ScrollMagic.Scene({
+   triggerElement:'#portfolio'
+ })
+ .setClassToggle('#portfolio','show')
+ .addTo(controller);
+
+ new ScrollMagic.Scene({
+   triggerElement:'#contact'
+ })
+ .setClassToggle('#contact,#contact-list','show')
+ .addTo(controller);
+
+
+ TweenLite.to($("#titulo-fronted,#titulo-aurora"),3,{alpha:1});
+
+ functionScroll('#homepage-start','#homepage');
+ functionScroll('#aboutme-start','#aboutme');
+ functionScroll('#skills-start','#skills');
+ functionScroll('#portfolio-start','#portfolio');
+ functionScroll('#contact-start','#container-contact-list');
+ functionScroll('#coverpage-start', '#coverpage');
+ functionScroll('#icon1-contacts','#contacts');
+ functionScroll('#icon2-contacts','#contacts');
 
 })(jQuery);
